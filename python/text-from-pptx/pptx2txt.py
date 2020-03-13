67e748104772408722c62c1ab21a74eee5fc6dea
jpeterson1823 <petersonjohn418@gmail.com>
2020-03-13 16:56:14 -0500

    Started formating settings on the pptx2txt.py file.

--- python/text-from-pptx/pptx2txt.py
+++ python/text-from-pptx/pptx2txt.py
@@ -9,8 +9,12 @@ text = file_data['content']
 text = re.sub("Click the mouse button or press the \nSpace Bar to display the information\.", "", text)
 text = re.sub("\(pages \d\d\d\d?–\d\d\d\d?\)", "", text)
 text = re.sub("\n*Presentation Plus!\n*Splash Screen\n*", "", text)
-text = re.sub("\n\n\n", "\n", text)
 text = re.sub("+", "", text)
+text = re.sub("[\n]+", "", text)
+text = re.sub("\(cont\.\)", "\n", text)
+text = re.sub("\S\t", "\g<0>\n\t", text)
+text = re.sub("Section \d\d?-\d\d?", "\n\n\g<0>\n", text)
+text = re.sub("\.[\s*]?", ".\n", text)
 
 notes = open("./notes.txt","w")
 notes.write(text)
