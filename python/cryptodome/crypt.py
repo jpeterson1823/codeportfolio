76d59c6d32d6f407699e07fabb9b40d16f4656e6
jpeterson1823 <petersonjohn418@gmail.com>
2020-03-13 19:59:59 -0500

    Added a encrypt/decrypt option for the program.

--- python/cryptodome/crypt.py
+++ python/cryptodome/crypt.py
@@ -1,16 +1,27 @@
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
-import binascii
+import binascii, sys
 
-pubkey = RSA.import_key(open("public.key", "rb").read())
-prvkey = RSA.import_key(open("private.key", "rb").read())
+if len(sys.argv) != 2:
+    print("Usage: ./crypt [-e, -d]")
+    exit(1)
 
-euser = PKCS1_OAEP.new(pubkey).encrypt(input("Username: ").encode())
-epass = PKCS1_OAEP.new(pubkey).encrypt(input("Password: ").encode())
+if sys.argv[1] == '-e':
+    pubkey = RSA.import_key(open("keys/public.key", "rb").read())
 
-f = open("login.info", "wb")
-f.write(binascii.hexlify(euser))
-f.write(b";")
-f.write(binascii.hexlify(epass))
-f.close()
+    euser = PKCS1_OAEP.new(pubkey).encrypt(input("Username: ").encode())
+    epass = PKCS1_OAEP.new(pubkey).encrypt(input("Password: ").encode())
 
+    f = open("login.info", "wb")
+    f.write(binascii.hexlify(euser) + b";" + binascii.hexlify(epass))
+    f.close()
+    print("Encrypted data and logged it to login.info.")
+    exit(0)
+
+elif sys.argv[1] == '-d':
+    pvtkey = RSA.import_key(open("keys/private.key", "rb").read())
+
+    login = [binascii.unhexlify(string) for string in open("login.info","r").read().split(";")]
+    user = PKCS1_OAEP.new(pvtkey).decrypt(login[0])
+    pswd = PKCS1_OAEP.new(pvtkey).decrypt(login[1])
+    print(user,pswd)
