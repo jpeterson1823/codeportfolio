af59c33ce28d22cc507bc6418d953d3148bbf28c
Invisa <petersonjohn418@gmail.com>
2020-05-11 17:08:36 -0500

    Added "Object of Your Affection" project files from codecademy.

---
+++ cpp/ooya/profile.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "profile.hpp"
+
+Profile::Profile(std::string new_name, int new_age, std::string new_city, std::string new_country, std::string new_pronouns)
+  : name(new_name), age(new_age), city(new_city), country(new_country), pronouns(new_pronouns) {}
+
+std::string Profile::view_profile() {
+
+  std::string bio = "Name: " + name;
+  bio += "\nAge: " + std::to_string(age);
+  bio += "\nPronouns: " + pronouns;
+  bio += "\nHobbies: ";
+  for (std::string hobby : hobbies) bio += "\n\t" + hobby;
+  return bio + "\n";
+
+}
+
+void Profile::add_hobby(std::string new_hobby) {
+  hobbies.push_back(new_hobby);
+}
