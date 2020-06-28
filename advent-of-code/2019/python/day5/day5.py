33bba5946f79cc4d70da7bca8613a7c5b966990a
jpeterson1823 <petersonjohn418@gmail.com>
2020-06-28 07:50:45 -0500

    Fixed bug in:         adventofcode/2019/python/day5 where the reading of the instruction containing the opcode and modes was not being converted into an integer, screwing the entire process up and causing a failure to read.

--- advent-of-code/2019/python/day5/day5.py
+++ advent-of-code/2019/python/day5/day5.py
@@ -1,75 +1,77 @@
 def readIntcode(intcode):
-    index = 0
-    while index < len(intcode):
-        opcode = intcode[index]%100
-        modec = intcode[index]/100%10
-        modeb = intcode[index]/1000%10
-        modea = intcode[index]/10000%10
+    # Pointer for intcode
+    pointer = 0
 
-        param1 = intcode[intcode[index+1]] if modec == 0 else intcode[index+1]
-        param2 = intcode[intcode[index+2]] if modeb == 0 else intcode[index+2]
-        param3 = intcode[intcode[index+3]] if modea == 0 else intcode[index+3]
+    while True:
+        inst = intcode[pointer]
+        opcode = int(inst%100)
+        c = int(inst/100%10)
+        b = int(inst/1000%10)
+        a = int(inst/10000%10)
+        
 
-        if opcode == 99:
-            index = len(intcode)
-
-        elif opcode == 1:
-            intcode[param3] = param1 + param2
-            index += 4
-
-        elif opcode == 2:
-            intcode[param3] = param1 * param2
-            index += 4
-
-        elif opcode == 3:
-            i = input('Input: ')
-            intcode[intcode[index+1]] = int(i)
-            index += 2
+        # Check for opcode 3 and 4 to avoid index overflow.
+        if opcode == 3:
+            intcode[intcode[pointer+1]] = int(input('Input: '))
+            pointer += 2 
 
         elif opcode == 4:
-            print('Output: {}'.format(intcode[intcode[index+1]]))
-            index += 2
-
-        elif opcode == 5:
-            if param1 != 0:
-                index = param2
-            else:
-                index += 3
-
-        elif opcode == 6:
-            if param1 == 0:
-                index = param2
-            else:
-                index += 3
-
-        elif opcode == 7:
-            if param1 < param2:
-                intcode[param3] = 1
-            else:
-                intcode[param3] = 0
-            index += 4
-
-        elif opcode == 8:
-            if param1 == param2:
-                intcode[param3] = 1
-            else:
-                intcode[param3] = 0
-            index += 4
-
+            print('Output: '+str(intcode[intcode[pointer+1]]))
+            pointer += 2
+        
+        
         else:
-            print('Error has occured.')
-            print('Opcode: {}\nparam1: {}\nparam2: {}\nparam3: {}\n'.format(opcode, param1, param2, param3))
-            index = len(intcode)
-    
-    print('Finished reading intcode')
+            if opcode == 99:
+                break
+
+            p1 = intcode[intcode[pointer+1]] if c == 0 else intcode[pointer+1]
+            p2 = intcode[intcode[pointer+2]] if b == 0 else intcode[pointer+2]
+            p3 = intcode[pointer+3] if a == 0 else pointer+3
+
+            if opcode == 1:
+                intcode[p3] = p1 + p2
+                pointer += 4
+
+            elif opcode == 2:
+                intcode[p3] = p1 * p2
+                pointer += 4
+
+            elif opcode == 5:
+                if p1 != 0:
+                    pointer = p2
+                else:
+                    pointer += 3
+
+            elif opcode == 6:
+                if p1 == 0:
+                    pointer = p2
+                else:
+                    pointer += 3
+
+            elif opcode == 7:
+                if p1 < p2:
+                    intcode[p3] = 1
+                else:
+                    intcode[p3] = 0
+                pointer += 4
+
+            elif opcode == 8:
+                if p1 == p2:
+                    intcode[p3] = 1
+                else:
+                    intcode[p3] = 0
+                pointer += 4
+
+            else:
+                print('Error in opcode at pointer='+str(pointer)+', opcode='+str(opcode))
+                break
 
 
 
-
+        
 def main():
     intcode = [int(x) for x in open('5.in').read().split(',')]
     readIntcode(intcode)
-
 
 if __name__ == '__main__':
     main()
